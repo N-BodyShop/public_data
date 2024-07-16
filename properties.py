fix radius() calculation to calculate R500, R200, etc

properties.py
@@ -178,20 +178,20 @@ class Radius(LivePropertyCalculation):
         return 'radius'
     
     def requires_property(self):
-        return ['tot_mass_profile']
+        return ['star_mass_profile', 'gas_mass_profile','dm_mass_profile']
     
     def live_calculate(self, halo,*args):
+        self._deltar = self.get_simulation_property("approx_resolution_kpc", 0.1) 
         ts = halo.timestep
         z = ts.redshift
         a = 1.0 / (1.0 + z)
         H_z = pynbody.analysis.cosmology._a_dot(a, self._h0, self._omegaM0, self._omegaL0) / a
         H_z = pynbody.units.Unit("100 km s^-1 Mpc^-1") * H_z
         rho_crit = (3 * H_z ** 2) / (8 * np.pi * pynbody.units.G)
-        if halo['tot_mass_profile'].max() == 0:
-            return None
-        else:
-            rho_mean = halo['tot_mass_profile']/(4./3. * np.pi * ((np.arange(len(halo['tot_mass_profile']))+1)*0.1)**3)
-            return np.where(rho_mean>rho_crit.in_units('Msol kpc**-3')*self._ncrit)[0][-1]*0.1
+
+        tot_mass_profile = halo['dm_mass_profile']+halo['gas_mass_profile']+halo['star_mass_profile']
+        rho_mean = tot_mass_profile/(4./3. * np.pi * ((np.arange(len(tot_mass_profile))+1)*self._deltar)**3)
+        return (np.where(rho_mean>rho_crit.in_units('Msol kpc**-3')*self._ncrit)[0][-1]+1)*self._deltar
 
 class StellarProfileFaceOn(PynbodyPropertyCalculation):
     '''
@@ -202,11 +202,11 @@ class StellarProfileFaceOn(PynbodyPropertyCalculation):
         return "v_surface_brightness", "b_surface_brightness", "i_surface_brightness"
 
     def plot_x0(cls):
-        return 0.05
+        return self.get_simulation_property("approx_resolution_kpc", 0.1)/2
 
     @classmethod
     def plot_xdelta(cls):
-        return 0.1
+        return self.get_simulation_property("approx_resolution_kpc", 0.1)
 
     @classmethod
     def plot_xlabel(cls):