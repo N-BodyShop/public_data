fix bugs in sersic calculation

properties.py
@@ -3,6 +3,7 @@ from tangos.properties.pynbody.centring import centred_calculation
 from tangos.properties import PropertyCalculation, LivePropertyCalculation
 import pynbody
 import numpy as np
+import scipy
 
 
 class Outflows(PynbodyPropertyCalculation):
@@ -264,12 +265,12 @@ class StellarProfileDiagnosis(LivePropertyCalculation):
     def requires_property(self):
         return ["v_surface_brightness", "b_surface_brightness", "i_surface_brightness", 'max_radius', 'shrink_center']
     
-    def sersic_surface_brightness(r, mueff, reff, n):
+    def sersic_surface_brightness(self,r, mueff, reff, n):
         # I(R) = m0 + 2.5*b_n/ln(10) ( (r/reff)^(1/n)-1 )
         #b_n taken based on solution to gamma function (Capaccioli 1989) for n < 10.
         return mueff + 2.5*(0.868*n-0.142)*((r/reff)**(1./n) - 1)
 
-    def fit_sersic(r, surface_brightness, return_cov=False):
+    def fit_sersic(self, r, surface_brightness, return_cov=False):
         s0_guess = np.mean(surface_brightness[:3])
         #s0_range = [s0_guess-2, s0_guess+2]
         #r0_range = [0.1,r[-1]]
@@ -284,7 +285,7 @@ class StellarProfileDiagnosis(LivePropertyCalculation):
         sigma = 10**(0.6*(surface_brightness-20))/r
         sigma = None
 
-        popt, pcov = scipy.optimize.curve_fit(sersic_surface_brightness,r,surface_brightness,
+        popt, pcov = scipy.optimize.curve_fit(self.sersic_surface_brightness,r,surface_brightness,
                                           bounds=np.array((s0_range, r0_range, n_range)).T,
                                           sigma=sigma,
                                           p0=(s0_guess, r0_guess, n_guess))
@@ -295,20 +296,20 @@ class StellarProfileDiagnosis(LivePropertyCalculation):
             return popt
 
     def live_calculate(self, halo, *args):
-        r0 = 0.05
-        delta_r = self.get_simulation_property("aprox_resolution_kpc", 0.1)
+        delta_r = self.get_simulation_property("approx_resolution_kpc", 0.1)
+        r0 = delta_r/2
         if self.band+"_surface_brightness" in halo.keys():
             surface_brightness = halo[self.band+"_surface_brightness"]
         else:
             try:
-                surface_brightness = halo.calculate(self.band+"_surface_brightness")
+                surface_brightness = halo.calculate(self.band+"_surface_brightness()")
             except NoResultsError:
                 return None, None, None, None
         if len(surface_brightness)<self.smooth*2:
             return None, None, None, None
         if surface_brightness.max()==0:
             return None, None, None, None
-        r = np.arange(len(surface_brightness))*delta_r + 0.05
+        r = np.arange(len(surface_brightness))*delta_r + delta_r/2.
         nbins = len(surface_brightness)
         if self.smooth > 1:
             surface_brightness_new = np.nanmean(np.pad(surface_brightness[self.smooth:].astype(float),
@@ -362,7 +363,7 @@ class StellarProfileDiagnosis(LivePropertyCalculation):
             return None, None, None, None
         else:
             try:
-                m0, r0, n = fit_sersic(r_fit, sb_fit)
+                m0, r0, n = self.fit_sersic(r_fit, sb_fit)
             except:
                 m0 = None
                 n = None