52da8bbf22c2c030a68cbc089d02a04f804bf8c0
Ben Keller <malzraa@gmail.com>
2025-04-17 08:41:08 -0500

    Got the first set of tests working with vscode's testing plugin woohoo!

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def build(simname):
-    return subprocess.run(['/bin/bash', '../build_tangos_DB.sh', simname, 'test.db'])
+    return subprocess.run(['/bin/bash', 'build_tangos_DB.sh', simname, 'test.db'])
 
 def test_build():
-    build('/home/kellerbw/data/testdata').check_returncode()
+    build('testdata').check_returncode()
--- tests/test_counts.py
+++ tests/test_counts.py
@@ -24,6 +24,6 @@ def test_property_count():
     """
     Make sure we have the correct number of properties
     """
-    N_import = len([line for line in open('../import_properties').readlines() if line[0] != '#'])
-    N_write = len([line for line in open('../write_properties').readlines() if line[0] != '#'])
+    N_import = len([line for line in open('import_properties').readlines() if line[0] != '#'])
+    N_write = len([line for line in open('write_properties').readlines() if line[0] != '#'])
     assert(len(tangos.get_simulation('g3021').timesteps[0].halos[0].keys()) == N_import + N_write)
