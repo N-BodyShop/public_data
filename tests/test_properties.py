4d80d5b68a20470f0182a6aa7e79b529b9a0670c
Ben Keller <malzraa@gmail.com>
2025-04-17 08:47:33 -0500

    Added the first test to check a loaded halo property.  3 down, 54 to go.

---
+++ tests/test_properties.py
@@ -0,0 +1,18 @@
+import os
+os.environ["TANGOS_DB_CONNECTION"] = "test.db"
+os.environ["TANGOS_SIMULATION_FOLDER"] = "testdata"
+import tangos
+import pynbody as pyn
+
+def test_masses():
+    """
+    Check that the tangos masses match the mass loaded straight from pynbody
+    """
+    for tsim in tangos.all_simulations():
+        snap = tsim.timesteps[0]
+        sim = pyn.load(snap.filename)
+        h = sim.halos()
+        assert(snap.halos[0]['finder_mass'] == h[0]['mass'].in_units('Msol').sum())
+        assert(snap.halos[0]['finder_star_mass'] == h[0].s['mass'].in_units('Msol').sum())
+        assert(snap.halos[0]['finder_gas_mass'] == h[0].g['mass'].in_units('Msol').sum())
+        assert(snap.halos[0]['finder_dm_mass'] == h[0].d['mass'].in_units('Msol').sum())
