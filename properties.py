9f749bfecbf4fc1001e62fd2d0b26b7e752194f7
mtremmel <m.tremmel6@gmail.com>
2025-05-09 13:39:00 +0100

    add BH_Mdot_histogram_ave to properties (as opposed to only the instantaneous accretion rates provided by the default tangos properties

--- properties.py
+++ properties.py
@@ -707,3 +707,57 @@ class VelDispersionProfileEncl(HaloDensityProfile):
         halo['vel'] += vcen
 
         return sigS, sigG, sigDM, sigS3d, sigG3d, sigDM3d
+
+class BHAccAveHistogram(TimeChunkedProperty):
+    @classmethod
+    def name(self):
+        return "BH_mdot_histogram_ave"
+
+    def requires_property(self):
+        return []
+
+
+    def preloop(self, f, db_timestep):
+        self.log = BHShortenedLog.get_existing_or_new(db_timestep.filename)
+
+    @classmethod
+    def no_proxies(self):
+        return True
+
+    def calculate(self, halo, properties):
+        tmax_Gyr = 20.0  # calculate up to 20 Gyr
+        nbins = int(20.0 / self.pixel_delta_t_Gyr)
+
+        halo = halo.s
+
+        if len(halo)!=1:
+            raise RuntimeError("Not a BH!")
+
+        if halo['tform'][0]>0:
+            raise RuntimeError("Not a BH!")
+
+        mask = self.log.vars['bhid']==halo['iord']
+        if(mask.sum()==0):
+            raise RuntimeError("Can't find BH in .orbit file")
+
+        t_orbit = self.log.vars['time'][mask]
+        Mdot_orbit = self.log.vars['mdotmean'][mask]
+        order = np.argsort(t_orbit)
+
+        t_max = properties.timestep.time_gyr
+        dt = self.tmax_Gyr/self.nbins
+        t_grid = np.arange(self.nbins+1)
+
+         #print t_grid
+        mdot_grid_n, _ = np.histogram(t_orbit[order],bins=nbins,range=(0,tmax_Gyr))
+        mdot_grid_sum, _ = np.histogram(t_orbit[order],weights=Mdot_orbit[order],bins=nbins,range=(0,tmax_Gyr))
+        mdot_grid_ave = mdot_grid_sum/mdot_grid_n.astype(np.float)
+
+
+         #Mdot_grid = scipy.interpolate.interp1d(t_orbit[order], Mdot_orbit[order], bounds_error=False)(t_grid)
+
+
+         #print t_max
+         #print Mdot_grid
+
+        return mdot_grid_ave[self.store_slice(t_max)]
