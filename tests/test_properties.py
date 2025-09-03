7b29ce4a5e516442fa0ec796c95fcfff092941fd
Ben Keller <malzraa@gmail.com>
2025-09-02 23:19:01 -0500

    Added some additional code to ensure environment variables are set when running tests locally.

--- tests/test_properties.py
+++ tests/test_properties.py
@@ -1,4 +1,6 @@
 import os
+os.environ["TANGOS_SIMULATION_FOLDER"] = os.path.realpath("testdata")
+os.environ["TANGOS_DB_CONNECTION"] = "test.db"
 import tangos
 import pynbody as pyn
 
@@ -6,7 +8,9 @@ def test_masses():
     """
     Check that the tangos masses match the mass loaded straight from pynbody
     """
+    ran = False
     for tsim in tangos.all_simulations():
+        ran = True
         snap = tsim.timesteps[0]
         sim = pyn.load(snap.filename)
         h = sim.halos()
@@ -14,3 +18,4 @@ def test_masses():
         assert(snap.halos[0]['finder_star_mass'] == h[0].s['mass'].in_units('Msol').sum())
         assert(snap.halos[0]['finder_gas_mass'] == h[0].g['mass'].in_units('Msol').sum())
         assert(snap.halos[0]['finder_dm_mass'] == h[0].d['mass'].in_units('Msol').sum())
+    assert(ran)
